28af8b907f474d39d25b234acebb22ca6863b282
Dio <diowong1@gmail.com>
2024-06-13 17:51:21 +0800

    Added Sign In Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,26 @@
-import React from 'react';
-
-import Header from './components/header.jsx';
-import Content from './components/dashboard.jsx';
-import Footer from './components/footer.jsx';
+import React, { useState } from 'react';
 import Landing from './pages/landing.jsx';
 import Planning from './pages/planning.jsx';
 import PlanningEdit from './pages/planning_edit.jsx';
 import Home from './pages/home.jsx';
 import Loading from './pages/loading.jsx';
 import NewPlan from './pages/home_new_plan.jsx';
+import SignIn from './pages/sign_in.jsx';
 
 function App() {
+    const [currentPage, setCurrentPage] = useState('Loading');
+
     return (
         <div>
-          {/* <BackgroundGradientAnimation>
-          </BackgroundGradientAnimation> */}
-          {/* <Landing/> */}
-          {/* <Planning/> */}
-          {/* <Home /> */}
-          {/* <Loading></Loading> */}
-          {/* <PlanningEditCopy/> */}
-          <PlanningEdit></PlanningEdit>
+            {currentPage === 'Landing' && <Landing />}
+            {currentPage === 'Planning' && <Planning />}
+            {currentPage === 'Home' && <Home />}
+            {currentPage === 'Loading' && <Loading />}
+            {currentPage === 'PlanningEdit' && <PlanningEdit />}
+            {currentPage === 'NewPlan' && <NewPlan />}
+            {currentPage === 'SignIn' && <SignIn />}
         </div>
     );
 }
 
 export default App;
-
-
