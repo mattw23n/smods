dffb0dbb002530c424fd8958549070d9ddf40217
Kang Taeho <kangtaeho2456@gmail.com>
2024-06-16 18:48:11 +0800

    Initial setup

--- src/main/java/com/smods/backend/controller/AuthController.java
+++ src/main/java/com/smods/backend/controller/AuthController.java
@@ -1,27 +1,24 @@
 package com.smods.backend.controller;
 
 import com.smods.backend.dto.LoginRequest;
-import com.smods.backend.model.User;
 import com.smods.backend.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
-    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
-        User user = userService.findByUsername(loginRequest.getUsername());
-        if (user != null && passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+        boolean isAuthenticated = userService.loginUser(loginRequest);
+        if (isAuthenticated) {
             return ResponseEntity.ok("Login successful");
         } else {
             return ResponseEntity.status(401).body("Invalid username or password");
