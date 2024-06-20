837cd71a258f5f935375cfabfa63bb1fbae18a16
Kang Taeho <kangtaeho2456@gmail.com>
2024-06-20 21:57:32 +0800

    Add exception for invalid characters

---
+++ src/main/java/com/smods/backend/exception/InvalidCharacterException.java
@@ -0,0 +1,7 @@
+package com.smods.backend.exception;
+
+public class InvalidCharacterException extends RuntimeException {
+    public InvalidCharacterException(String message) {
+        super(message);
+    }
+}
