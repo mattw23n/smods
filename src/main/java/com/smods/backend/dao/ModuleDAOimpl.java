a13f296495b686b2edc99ca0d6fb2f514704eb6b
gilchris <gilchrisnathaniel@gmail.com>
2024-06-22 23:09:10 +0700

    hehe

---
+++ src/main/java/com/smods/backend/dao/ModuleDAOimpl.java
@@ -0,0 +1,2 @@
+package com.smods.backend.dao;public class ModuleDAOimpl {
+}
--- src/main/java/com/smods/backend/exception/PlanModificationException.java
+++ src/main/java/com/smods/backend/exception/PlanModificationException.java
@@ -1,5 +1,6 @@
 package com.smods.backend.model;
 
+import java.lang.*;
 public class PlanModificationException extends RuntimeException{
 
     public PlanModificationException(){
