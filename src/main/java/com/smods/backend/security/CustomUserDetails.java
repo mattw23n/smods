a45d1cf034a4260c09852c056994375c5b385150
Kang Taeho <kangtaeho2456@gmail.com>
2024-07-01 22:15:28 +0800

    Add security configuration

---
+++ src/main/java/com/smods/backend/security/CustomUserDetails.java
@@ -0,0 +1,60 @@
+package com.smods.backend.security;
+
+import com.smods.backend.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    public Long getId() {
+        return user.getId();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(() -> user.getRole());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.getEmailVerified();
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
