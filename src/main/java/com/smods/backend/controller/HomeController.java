619898d13e13c813175e2a472cb0ae786cd39ace
Kang Taeho <kangtaeho2456@gmail.com>
2024-07-01 21:54:56 +0800

    Update Homepage endpoints

--- src/main/java/com/smods/backend/controller/HomeController.java
+++ src/main/java/com/smods/backend/controller/HomeController.java
@@ -1,13 +1,53 @@
 package com.smods.backend.controller;
 
+import com.smods.backend.service.HomeService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/home")
 public class HomeController {
 
+    private final HomeService homeService;
+
+    public HomeController(HomeService homeService) {
+        this.homeService = homeService;
+    }
+
     @GetMapping("/")
-    public String welcome() {
-        return "(☞ﾟヮﾟ)☞ welcome to smods ☜(ﾟヮﾟ☜)<br>".repeat(69);
+    public ResponseEntity<String> getWelcomeMessage() {
+        return ResponseEntity.ok(homeService.getWelcomeMessage());
+    }
+
+    @GetMapping("/summary")
+    public ResponseEntity<String> getSummary() {
+        return ResponseEntity.ok(homeService.getSummary());
+    }
+
+    @GetMapping("/about")
+    public ResponseEntity<String> getAboutUs() {
+        return ResponseEntity.ok(homeService.getAboutUs());
+    }
+
+    @GetMapping("/contact")
+    public ResponseEntity<String> getContactUs() {
+        return ResponseEntity.ok(homeService.getContactUs());
+    }
+
+    @GetMapping("/plans")
+    public ResponseEntity<String> planNow() {
+        return ResponseEntity.ok(homeService.planNow());
+    }
+
+    @GetMapping("/login")
+    public ResponseEntity<String> login() {
+        return ResponseEntity.ok(homeService.login());
+    }
+
+    @GetMapping("/register")
+    public ResponseEntity<String> register() {
+        return ResponseEntity.ok(homeService.register());
     }
 }
