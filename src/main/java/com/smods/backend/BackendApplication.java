26de31ba90b183c5d7a5f425a5ca512649c65395
Kang Taeho <kangtaeho2456@gmail.com>
2024-07-02 11:46:20 +0800

    Exclude spring security

--- src/main/java/com/smods/backend/BackendApplication.java
+++ src/main/java/com/smods/backend/BackendApplication.java
@@ -3,8 +3,9 @@ package com.smods.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class} )
 public class BackendApplication {
 
 	public static void main(String[] args) {
