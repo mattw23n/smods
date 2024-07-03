bc866c24258ae94f1b4973e532b3847961362f2f
Matt <matteo.woenardi@gmail.com>
2024-07-03 15:38:31 +0800

    Deleted unused pages and images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
 import Landing from './pages/landing.jsx';
 import Planning from './pages/planning.jsx';
-import PlanningEdit from './pages/planning_edit.jsx';
 import Home from './pages/home.jsx';
 import Loading from './pages/loading.jsx';
 import NewPlan from './pages/home-new-plan.jsx';
 import SignIn from './pages/sign_in.jsx';
-import PlanningGroup from './pages/planning_group.jsx';
-import PlanningYear from './pages/planning_year.jsx';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
