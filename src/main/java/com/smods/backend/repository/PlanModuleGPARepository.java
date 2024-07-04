21c89bbdc2f49425cd60cc98cf8e357a39128b25
gilchris <gilchrisnathaniel@gmail.com>
2024-07-04 23:49:44 +0700

    remove unnecessary classes, addnew methods in ModuleRepository and PlanModuleGPARepository and recreate addModule in PlanService

--- src/main/java/com/smods/backend/repository/PlanModuleGPARepository.java
+++ src/main/java/com/smods/backend/repository/PlanModuleGPARepository.java
@@ -1,9 +1,29 @@
 package com.smods.backend.repository;
 
+import com.smods.backend.model.Module;
 import com.smods.backend.model.PlanModuleGPA;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.smods.backend.model.composite_key.PlanModuleGPAKey;
+
+import java.util.List;
+
 @Repository
 public interface PlanModuleGPARepository extends JpaRepository<PlanModuleGPA, PlanModuleGPAKey> {
+
+    @Query("SELECT p FROM PlanModuleGPA p " +
+            "WHERE p.planModuleGPAId.planId = :#{#planModuleGPAId.planId} " +
+            "AND p.term < :term")
+    public List<Module> findAllPlanModulesByIdBeforeTerm(@Param("planModuleGPAId") PlanModuleGPAKey planModuleGPAId, @Param("term") int term);
+
+    @Query("SELECT p FROM PlanModuleGPA p " +
+            "WHERE p.planModuleGPAId.planId = :#{#planModuleGPAId.planId} " +
+            "AND p.term = :term")
+    public List<Module> findAllModulesByPlanIdAndTerm(@Param("planModuleGPAId") PlanModuleGPAKey planModuleGPAId, @Param("term") int term);
+
+    @Query("SELECT p FROM PlanModuleGPA p " +
+            "WHERE p.planModuleGPAId.planId = :#{#planModuleGPAId.planId} ")
+    public List<Module> findAllModulesByPlanId(@Param("planModuleGPAId") PlanModuleGPAKey planModuleGPAId);
 }
