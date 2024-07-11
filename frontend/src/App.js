e88c725873f4d70e876013d5b9072d70261bea35
Kang Taeho <kangtaeho2456@gmail.com>
2024-07-11 22:58:49 +0800

    Redirect to /home after email verification

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from './pages/landing.jsx';
 import Planning from './pages/planning.jsx';
 import Home from './pages/home.jsx';
-import Loading from './pages/loading.jsx';
 import NewPlan from './pages/home-new-plan.jsx';
 import SignIn from './pages/sign_in.jsx';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import {UserProvider} from "./data/user";
+import Register from './pages/register.jsx';
 import AboutUs from './pages/aboutUs.jsx';
 import ContactUs from './pages/contactUs.jsx';
 import Anims from './pages/justForAnims.jsx';
-import Register from "./pages/register";
-import VerifyEmail from "./pages/VerifyEmail";
-import ResendVerification from "./pages/ResendVerification";
+import VerifyEmail from './pages/VerifyEmail.jsx';
+import ResendVerification from './pages/ResendVerification.jsx';
+import HandleVerification from './pages/HandleVerification.jsx';
+import { UserProvider } from "./data/user";
 
 function App() {
-    
-
     return (
         <UserProvider>
             <Router>
                 <Routes>
-                    <Route path="/home" element={ <Home />} />
-                    <Route exact path="/" element={ <Landing />} />
-                    <Route path="/new-plan" element={ <NewPlan />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route exact path="/" element={<Landing />} />
+                    <Route path="/new-plan" element={<NewPlan />} />
                     <Route path="/plan/:id" element={<Planning />} />
                     <Route path="/signin" element={<SignIn />} />
                     <Route path="/register" element={<Register />} />
@@ -32,11 +30,11 @@ function App() {
                     <Route path="/anims" element={<Anims />} />
                     <Route path="/verify-email" element={<VerifyEmail />} />
                     <Route path="/resend-verification" element={<ResendVerification />} />
+                    <Route path="/handle-verification" element={<HandleVerification />} />
                 </Routes>
             </Router>
         </UserProvider>
     );
-    
 }
 
 export default App;
