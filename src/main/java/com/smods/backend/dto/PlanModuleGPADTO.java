e4cae2c35fe4912595f1b5f10b72c6d0619e6119
Kang Taeho <kangtaeho2456@gmail.com>
2024-07-20 17:55:45 +0800

    Update PlanModuleGPADTO

--- src/main/java/com/smods/backend/dto/PlanModuleGPADTO.java
+++ src/main/java/com/smods/backend/dto/PlanModuleGPADTO.java
@@ -2,24 +2,32 @@ package com.smods.backend.dto;
 
 public class PlanModuleGPADTO {
     private String moduleId;
+    private String moduleName;
     private double gpa;
     private int term;
 
-    // Constructor, getters, and setters
-    public PlanModuleGPADTO(String moduleId, double gpa, int term) {
+    public PlanModuleGPADTO(String moduleId, String moduleName, double gpa, int term) {
         this.moduleId = moduleId;
+        this.moduleName = moduleName;
         this.gpa = gpa;
         this.term = term;
     }
 
     // Getters and setters
-
     public String getModuleId() {
         return moduleId;
     }
 
     public void setModuleId(String moduleId) {
         this.moduleId = moduleId;
+    }
+
+    public String getModuleName() {
+        return moduleName;
+    }
+
+    public void setModuleName(String moduleName) {
+        this.moduleName = moduleName;
     }
 
     public double getGpa() {
@@ -38,4 +46,3 @@ public class PlanModuleGPADTO {
         this.term = term;
     }
 }
-
